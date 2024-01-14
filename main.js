b2b4d1aa681356c0e5ca2d181a66674ce2754638
rajat230522 <106083365+rajat230522@users.noreply.github.com>
2024-01-15 01:11:53 +0530

    updated

--- main.js
+++ main.js
@@ -9,3 +9,29 @@ document.addEventListener('DOMContentLoaded', function () {
     //         myModal.hide();
     // });
 });
+$(document).ready(function () {
+    // Intercept the form submission
+    $('form').submit(function (e) {
+        e.preventDefault(); // Prevent the default form submission
+
+        // Send the form data to Formspree via AJAX
+        $.ajax({
+            url: 'https://formspree.io/f/mkndgbwa',
+            method: 'POST',
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function () {
+                // Handle success, e.g., show a thank you message
+                alert('Form submitted successfully!');
+                $('#popupForm').modal('hide');
+
+                // Clear the form fields if needed
+                document.getElementById("form").reset();
+            },
+            error: function () {
+                // Handle error, e.g., show an error message
+                alert('Error submitting the form.');
+            }
+        });
+    });
+});
